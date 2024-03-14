ccca4f55ecdd325c59fe2452ded71c57a4e01407
Tymofii Maksymenko <tim.off.max@gmail.com>
2024-03-14 09:02:35 +0200

    Fix regex to ignore cyrillic symbols between the searched chars

--- model/api/response/utils/message_utils.go
+++ model/api/response/utils/message_utils.go
@@ -6,7 +6,7 @@ import (
 )
 
 func IsAnusMessage(message *response.TgMessage) bool {
-	regex := regexp.MustCompile(`(?i)(очко|([a@ая])([^a-zA-Z0-9]+?)?([nнh])([^a-zA-Z0-9]+?)?([uуy])([^a-zA-Z0-9]+?)?([sсc$]))`)
+	regex := regexp.MustCompile(`(?i)(очко|([a@ая])([^a-zA-Z0-9А-Яа-я]+?)?([nнh])([^a-zA-Z0-9А-Яа-я]+?)?([uуy])([^a-zA-Z0-9А-Яа-я]+?)?([sсc$]))`)
 
 	return regex.MatchString(message.Text)
 }
